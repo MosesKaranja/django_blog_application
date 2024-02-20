e920a7a079b5265f1cf9ad2b39ee96a62496151c
MosesKaranja <moseskaranja830@gmail.com>
2024-02-20 23:20:29 +0300

    profile picture

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -27,4 +27,13 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('blog/', include('blog.urls'))
+
 ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
