4761bfdff1d436413b0b761c05c3c95b3f5b6362
MosesKaranja <moseskaranja830@gmail.com>
2024-02-21 02:30:00 +0300

    changes to profile pic

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -16,4 +17,9 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
         
